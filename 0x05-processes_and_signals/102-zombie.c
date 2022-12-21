f3c5bf6e6967c3ee69d480675e5f075db71fa209
Chris Stamper <cstampok@gmail.com>
2022-12-21 16:06:43 -0600

    zombie factory

--- 0x05-processes_and_signals/102-zombie.c
+++ 0x05-processes_and_signals/102-zombie.c
@@ -1,13 +1,38 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
-int main()
+/**
+ * infinite_while - Function that runs an infinite loop
+ * Return: Always 0
+ */
+int infinite_while(void)
 {
-    int zid; /*zombieID*/
-    int loopi = 1;
-    while (loopi < 6)
-    {
-        printf("Zombie process created, PID: %d\n", zid);
-        loopi++;
-        sleep(1);
-    }
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
+}
+
+/**
+ * main - zombie factory
+ * Return: Always 0
+ */
+int main(void)
+{
+	pid_t pidZ;
+	int i;
+
+	for (i = 0; i < 5; i++)
+	{
+		pidZ = fork();
+		if (pidZ == 0)
+		{
+			printf("Zombie process created, PID: %d\n", getpid());
+			exit(0);
+		}
+	}
+	infinite_while();
+	return (0);
 }
