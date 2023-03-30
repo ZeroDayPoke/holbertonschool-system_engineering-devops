95d744daca1a1c963ba35a6e839e8708e197ac92
CStamper <cstampok@gmail.com>
2023-03-30 14:05:21 -0500

    format string and pycode style fixes

--- 0x0E-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x0E-api/3-dictionary_of_list_of_dictionaries.py
@@ -2,19 +2,25 @@
 """doc"""
 import requests
 import json
+import hashlib
+
 
 def get_all_employees():
+    """doc"""
     base_url = "https://jsonplaceholder.typicode.com"
-    
-    user_response = requests.get(f"{base_url}/users")
+
+    user_response = requests.get("{}/users".format(base_url))
     user_data = user_response.json()
 
     return user_data
 
+
 def get_employee_tasks(user_id, username):
+    """doc"""
     base_url = "https://jsonplaceholder.typicode.com"
-    
-    todos_response = requests.get(f"{base_url}/users/{user_id}/todos")
+
+    todos_response = requests.get("{}/users/{}/todos"
+                                  .format(base_url, user_id))
     todos_data = todos_response.json()
 
     task_data = []
@@ -27,9 +33,11 @@ def get_employee_tasks(user_id, username):
 
     return task_data
 
+
 def export_to_json(all_employees_data):
+    """doc"""
     file_name = "todo_all_employees.json"
-    
+
     all_tasks = {}
     for employee in all_employees_data:
         user_id = employee["id"]
@@ -40,7 +48,8 @@ def export_to_json(all_employees_data):
     with open(file_name, "w") as jsonfile:
         json.dump(all_tasks, jsonfile)
 
-    print(f"Data exported to {file_name}")
+    print("Data exported to {}".format(file_name))
+
 
 if __name__ == "__main__":
     all_employees_data = get_all_employees()
