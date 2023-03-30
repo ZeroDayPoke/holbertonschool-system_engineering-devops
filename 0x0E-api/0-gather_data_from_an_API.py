f37a46d100066c6b3c788f33fbc121af9b8dfa2d
CStamper <cstampok@gmail.com>
2023-03-30 13:37:38 -0500

    pycodestyle fixes

--- 0x0E-api/0-gather_data_from_an_API.py
+++ 0x0E-api/0-gather_data_from_an_API.py
@@ -3,10 +3,11 @@
 import sys
 import requests
 
+
 def get_employee_todo_progress(employee_id):
     """doc"""
     base_url = "https://jsonplaceholder.typicode.com"
-    
+
     user_response = requests.get(f"{base_url}/users/{employee_id}")
     user_data = user_response.json()
 
@@ -19,11 +20,13 @@ def get_employee_todo_progress(employee_id):
 
     done_tasks = [task for task in todos_data if task["completed"]]
     total_tasks = len(todos_data)
-    
-    print(f"Employee {user_data['name']} is done with tasks({len(done_tasks)}/{total_tasks}):")
-    
+
+    print(f"Employee {user_data['name']} is done
+          with tasks({len(done_tasks)}/{total_tasks}): ")
+
     for task in done_tasks:
         print("\t", task["title"])
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
